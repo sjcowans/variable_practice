bd4cff0be3cf707ab064542e15391201e9922319
Sean Cowans <zedesia@yahoo.com>
2023-01-03 12:32:52 -0700

    Add puts

--- variables.rb
+++ variables.rb
@@ -3,7 +3,21 @@ maximum_sprint_speed = 9000
 height = "6'1 but on the money 5'9" #I shrink bc of that nice debt
 debt = 360,000
 depression = 99999999
-understanding = .0001
-competence = .00005
+understanding = 0.0001
+competence = 0.00005
 finished = false
 having_fun = true
+
+name = "Sean the Bomb"
+maximum_sprint_speed = 2
+height = "6'1 but only sometimes"
+
+puts "Hi y'all!"
+
+finished = true
+
+if finished == true 
+    puts "I did it!" 
+elsif finished == false
+    puts "boooo"
+end
